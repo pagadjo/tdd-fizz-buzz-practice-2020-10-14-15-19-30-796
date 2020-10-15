e7e37e0dd25486338af6710c66a1b3a7ed19f5b5
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-15 14:52:42 +0800

    [FizzBuzzWhizz][John Carlos]refactor: return BuzzWhizz when orderNum is multiple to 5,7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -11,7 +11,7 @@ public class FizzBuzz {
         final boolean isModulo7 = (orderNumber % 7 == 0);
 
         if (isModulo5 && isModulo7) {
-            return String.valueOf(orderNumber);
+            return BUZZ + WHIZZ;
         }
         if (isModulo3 && isModulo7) {
             return FIZZ + WHIZZ;
