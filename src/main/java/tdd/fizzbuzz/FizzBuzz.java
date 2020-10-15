cb7e2ffc9a2b5eaf5646761a5ba4cabf1b7c2979
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-15 14:29:11 +0800

    [FizzBuzzWhizz][John Carlos]feature: return Whizz when orderNum is multiple to 7 and extracted FIZZ and isModulo7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,13 +3,15 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
     private final String FIZZ = "Fizz";
     private final String BUZZ = "Buzz";
+    private final String WHIZZ = "Whizz";
 
     public String countOff(int orderNumber) {
         final boolean isModulo3 = (orderNumber % 3 == 0);
         final boolean isModulo5 = (orderNumber % 5 == 0);
+        final boolean isModule7 = (orderNumber % 7 == 0);
 
-        if (orderNumber % 7 == 0) {
-            return String.valueOf(orderNumber);
+        if (isModule7) {
+            return WHIZZ;
         }
         if (isModulo5) {
             return BUZZ;
