07044dd6a2592e5274d802cf33bc42fb5c7f19c9
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-15 14:17:43 +0800

    [FizzBuzzWhizz][John Carlos]feature: return Buzz when orderNum is multiple to 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,8 +2,12 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     private final String FIZZ = "Fizz";
+
     public String countOff(int orderNumber) {
         final boolean isModulo3 = (orderNumber % 3 == 0);
+        if (orderNumber % 5 == 0) {
+            return "Buzz";
+        }
         if (isModulo3) {
             return FIZZ;
         }
