6fb4589627f8b66e2c90ba1b981cd7b9e23b6c28
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-15 14:20:07 +0800

    [FizzBuzzWhizz][John Carlos]refactor: extract BUZZ and isModulo5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,11 +2,13 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     private final String FIZZ = "Fizz";
+    private final String BUZZ = "Buzz";
 
     public String countOff(int orderNumber) {
         final boolean isModulo3 = (orderNumber % 3 == 0);
-        if (orderNumber % 5 == 0) {
-            return "Buzz";
+        final boolean isModulo5 = (orderNumber % 5 == 0);
+        if (isModulo5) {
+            return BUZZ;
         }
         if (isModulo3) {
             return FIZZ;
