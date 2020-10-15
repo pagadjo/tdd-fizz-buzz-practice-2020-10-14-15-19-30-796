327c0df0755cfefd3b8e86886a045593c95a0ae7
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-15 13:59:58 +0800

    [FizzBuzzWhizz][John Carlos]test: implement should return 1 when order number is 1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,21 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_return_1_when_order_number_is_1() {
+        //given
+        String actual = "";
+        int orderNumber = 0;
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        //when
+        actual = fizzbuzz.countOff(orderNumber);
+        //then
+        assertEquals("1", actual);
+    }
+
 }
