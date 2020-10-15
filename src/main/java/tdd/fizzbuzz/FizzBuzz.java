e27a3c437b667afa839fd1b11d23f554e29d93b8
PAGADJO <john.carlos.pagaduan@oocl.com>
2020-10-15 14:39:33 +0800

    [FizzBuzzWhizz][John Carlos]refactor: used final String FIZZ and WHIZZ for output

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -11,7 +11,7 @@ public class FizzBuzz {
         final boolean isModulo7 = (orderNumber % 7 == 0);
 
         if (isModulo3 && isModulo7) {
-            return "FizzWhizz";
+            return FIZZ + WHIZZ;
         }
         if (isModulo3 && isModulo5) {
             return FIZZ + BUZZ;
